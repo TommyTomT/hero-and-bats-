f1479778c81f128516ca0fc4721189412d1549e4
TommyTomT <79381169+TommyTomT@users.noreply.github.com>
2022-02-26 11:34:55 -0500

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000200001050505050505050505050505050502080a0e0e0e0e10100e0e0e0e0e0e0e06080b0d0d0e0e0e0e0e0e0e0e0e0e0e06080b0d0d0d0d0d0d0d0e0e100e0e0e06080b0e0e0e0e0e0e0e0e0e0e0e0e0e06080b0e0e100e0e0e0e0e0e0d0d0d0e06080b0e0e100e0e0d0d0d0d0d0e0d0e06080b0e0e0e0e0e0e0e0e0e0e0e0e0e06080b0e0e0e0e0e0e0e0e100e0e0e0e06080b0d0d0d0e0e0e0e0e0e0e0e100e06080b0d0d0d0d0d0d0d0d0e0e0e0e0e06070b0e0e0e0e0e0d0d0d0d0e100e1006080b100e0e0e0e0e0e0e0e0e0e0e0e06080b0e0d0d0d0d0d0d0d0d0d0d0d0d06080b0e0e0e0e0e0e0e0e0e0e0e0e0e06080b0d0d0d0d0d0d0d0d0e100e0e0e06080b0e0e0e0e0e0e0e0d0e0e0e0e1006080b0e0e100e100e0e0d0d0d0e0e0e06070b0e0e100e0e0e0e0e0e0d0d0e0e06070b0e0e0e0e0e0e0e0e0e0e0d0d0e06080b0e0e100e0e0e0e0e0e0e0d0e0e06080b0e0e0e0e0e100e0e0e0e0e0e0e06080b0e0e0e0e0e0e0e0e0e0e100e0e06080b0e0d0e0e0e0e0e0d0d0d0d0d0d06080b0e0d0d0d0d0d0d0d0d0e0e0e0e06080b0e0e0e0e0e0e0e0e0e0e0e100e06080b0e100e0e0e0e0e0e0e0e100e0e06080b0d0d0d0d0e0e0e100e0e0e0e0e06080b0e0e0e0e0e0e0e0e0e0e0e100e06080b0e0e0e0d0d0e0e0e0e0e0e0e0e06070c100e0e0f0d0d0d0d0d0d0d0d0d0604090909090909090909090909090903`, img`
+2222222222222222
+22.............2
+2222...........2
+222222222......2
+22.............2
+22.........222.2
+22.....22222.2.2
+22.............2
+22.............2
+22222..........2
+2222222222.....2
+22.....2222....2
+22.............2
+22.2222222222222
+22.............2
+2222222222.....2
+22.......2.....2
+22.......222...2
+22.........22..2
+22..........22.2
+22..........2..2
+22.............2
+22.............2
+22.2.....2222222
+22.22222222....2
+22.............2
+22.............2
+222222.........2
+22.............2
+22...22........2
+22....2222222222
+2222222222222222
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth0,sprites.builtin.forestTiles5,sprites.builtin.forestTiles9,sprites.builtin.forestTiles13,sprites.dungeon.floorDark0,sprites.dungeon.floorLight4,sprites.dungeon.chestClosed,sprites.dungeon.floorLightMoss], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
